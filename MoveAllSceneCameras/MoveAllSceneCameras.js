35ec33312611046b150af5f4ee3e922c9cf59cba
Josh Goldstein <jdeangoldstein@gmail.com>
2017-05-02 18:03:16 -0600

    Final touches

--- MoveAllSceneCameras/MoveAllSceneCameras.js
+++ MoveAllSceneCameras/MoveAllSceneCameras.js
@@ -23,53 +23,56 @@ var scenes = FormIt.Scenes.GetScenes();
 console.log("--------");
 console.log(JSON.stringify(scenes));*/
 
-console.log("Number of Scenes = " + scenes.length);
+console.log("Number of Scenes: " + scenes.length + '\n');
 
 for (i = 0; i < scenes.length; i++) {
 
 	// Get data for single scene
 	var scene = scenes[i];
-	console.log(scene);
-	console.log(JSON.stringify(scene));
 
 	// Get camera data
 	var camera = scene.camera;
 
-	console.log("--------");
-	console.log(JSON.stringify(camera));
+	// Get scene name
+	var name = scene.name;
 
 	// Get X value
 	var X = camera.posX;
 
-	console.log("--------");
-	console.log("X value: " + (JSON.stringify(X)));
+	console.log("--------" + '\n');
+	console.log("Scene Name: " + JSON.stringify(name) + '\n');
+	console.log("Original X value: " + (JSON.stringify(X)));
 
 	// Add value to X
 	X = X + deltaX;
 	camera.posX = X;
 
+	console.log("New X value: " + (JSON.stringify(X) + '\n'));
+
 	// Get Y value
 	var Y = camera.posY;
 
-	console.log("--------");
-	console.log("Y value: " + (JSON.stringify(Y)));
+	console.log("Original Y value: " + (JSON.stringify(Y)));
 
 	// Add value to Y
 	Y = Y + deltaY;
 	camera.posY = Y;
 
+	console.log("New Y value: " + (JSON.stringify(Y) + '\n'));
+
 	// Get Z value
 	var Z = camera.posZ;
 
-	console.log("--------");
-	console.log("Z value: " + (JSON.stringify(Z)));
+	console.log("Original Z value: " + (JSON.stringify(Z)));
 
 	// Add value to Z
 	Z = Z + deltaZ;
 	camera.posZ = Z;
 
-	console.log("--------");
-	console.log(JSON.stringify(camera));
+	console.log("New Z value: " + (JSON.stringify(Z)));
+	console.log('\n' + "--------" + '\n');
+
+	/*console.log(JSON.stringify(camera));*/
 	}
 
 	FormIt.Scenes.SetScenes(scenes);
@@ -87,7 +90,7 @@ deanstein.Submit = function()
     }
     console.log("deanstein.MoveCameras");
     console.log("args");
-    // NOTE: window.FormItInterface.CallMethod will call the CreateBlock function
+    // NOTE: window.FormItInterface.CallMethod will call the MoveCameras function
     // defined above with the given args.  This is needed to communicate
     // between the web JS enging process and the FormIt process.
     window.FormItInterface.CallMethod("deanstein.MoveCameras", JSON.stringify(args));

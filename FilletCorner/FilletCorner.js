8f62c7759a75e0f03c4669ff46299723cb4ef663
Josh Goldstein <josh.goldstein@autodesk.com>
2017-07-26 21:31:34 -0600

    Adding safeguard to not run plugin if nothing is selected

--- FilletCorner/FilletCorner.js
+++ FilletCorner/FilletCorner.js
@@ -9,44 +9,47 @@ console.log("Current history: " + JSON.stringify(nHistoryID));
 currentSelection = FormIt.Selection.GetSelections();
 console.log("Current selection: " + JSON.stringify(currentSelection));
 
-// get vertexID of the selection
-nVertexType = WSM.nVertexType;
-nVertexID = currentSelection[0]["ids"][0]["Object"];
-console.log("Vertex ID of current selection: " +  JSON.stringify(nVertexID));
+if (currentSelection)
+{
+    // get vertexID of the selection
+    nVertexType = WSM.nVertexType;
+    nVertexID = currentSelection[0]["ids"][0]["Object"];
+    console.log("Vertex ID of current selection: " +  JSON.stringify(nVertexID));
 
-// define point0
-point0 = WSM.APIGetVertexPoint3dReadOnly(nHistoryID,nVertexID);
-console.log("Point 0 = " + JSON.stringify(point0));
+    // define point0
+    point0 = WSM.APIGetVertexPoint3dReadOnly(nHistoryID,nVertexID);
+    console.log("Point 0 = " + JSON.stringify(point0));
 
-pointX0 = point0["x"];
-console.log("Point X0 = " + JSON.stringify(pointX0));
-pointY0 = point0["y"];
-console.log("Point Y0 = " + JSON.stringify(pointY0));
-pointZ0 = point0["z"];
-console.log("Point Z0 = " + JSON.stringify(pointZ0));
+    pointX0 = point0["x"];
+    console.log("Point X0 = " + JSON.stringify(pointX0));
+    pointY0 = point0["y"];
+    console.log("Point Y0 = " + JSON.stringify(pointY0));
+    pointZ0 = point0["z"];
+    console.log("Point Z0 = " + JSON.stringify(pointZ0));
 
-// get edges attached to point0
-nEdgeType = WSM.nEdgeType;
-edgeIDArray = WSM.APIGetObjectsByTypeReadOnly(nHistoryID,nVertexID,nEdgeType,true);
-console.log("Edge IDs attached to this vertex: " +  JSON.stringify(edgeIDArray));
+    // get edges attached to point0
+    nEdgeType = WSM.nEdgeType;
+    edgeIDArray = WSM.APIGetObjectsByTypeReadOnly(nHistoryID,nVertexID,nEdgeType,true);
+    console.log("Edge IDs attached to this vertex: " +  JSON.stringify(edgeIDArray));
 
-// TODO: throw an error if more than 2 are selected
-numberOfEdges = getEdgeIDs.length;
-console.log("Number of attached edges: " + numberOfEdges);
+    // TODO: throw an error if more than 2 are selected
+    numberOfEdges = getEdgeIDs.length;
+    console.log("Number of attached edges: " + numberOfEdges);
 
-// for each edge, get the vertex IDs
-for (i = 0; i <= numberOfEdges - 1; i++)
-    {
-    nType = WSM.nVertexType;
-    getVertexIDs = WSM.APIGetObjectsByTypeReadOnly(nHistoryID,edgeIDArray[i],nType,false);
-    console.log("Reading these vertex IDs from edges: " + JSON.stringify(getVertexIDs));
-    edgeVertexArray = ["edge" + i]
-    edgeIDi = "edgeID" + i;
-    }
+    // for each edge, get the vertex IDs
+    for (i = 0; i <= numberOfEdges - 1; i++)
+        {
+        nType = WSM.nVertexType;
+        getVertexIDs = WSM.APIGetObjectsByTypeReadOnly(nHistoryID,edgeIDArray[i],nType,false);
+        console.log("Reading these vertex IDs from edges: " + JSON.stringify(getVertexIDs));
+        edgeVertexArray = ["edge" + i]
+        edgeIDi = "edgeID" + i;
+        }
 
+    edgeIDArray.foreach(getVertexIDs);
+}
 
-edgeIDArray.foreach(getVertexIDs);
-
+/*
 getCurrentSelectionObjectValue = function(numberOfObjectsSelected) 
 {
     for (i = 0; i <= numberOfEdges - 1; i++) 
@@ -76,4 +79,4 @@ getCurrentSelectionObjectValue = function(numberOfObjectsSelected)
 
 getCurrentSelectionObjectValue(numberOfObjectsSelected);
 
-FormIt.Selection.ClearSelections();
+FormIt.Selection.ClearSelections();*/

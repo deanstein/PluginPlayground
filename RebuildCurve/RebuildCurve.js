a0086950edd15355f2c8a09ad887428a132be3ea
Josh Goldstein <jdeangoldstein@gmail.com>
2017-11-24 23:59:29 -0700

    Finishing next phase of Rebuild Curve plugin
    And also adding it back to the Plugin Playground listing.

--- RebuildCurve/RebuildCurve.js
+++ RebuildCurve/RebuildCurve.js
@@ -1,6 +1,6 @@
 deanstein = {};
 
-deanstein.RebuildArcCircle = function(args)
+deanstein.RebuildCurve = function(args)
 {
 
     console.clear();
@@ -17,31 +17,49 @@ deanstein.RebuildArcCircle = function(args)
     var currentSelectionLength = currentSelection.length;
     console.log("Number of objects in selection: " + currentSelectionLength);
 
-    var arcCircleAnalysisArray = new Array();
+    if (currentSelection.length === 0)
+    {
+        console.log("Select a line, arc, circle, or spline to begin.");
+        return;
+    }
+
     var typeArray = new Array();
+    var nObjectIDArray = new Array();
+    var nVertexIDArray = new Array();
     var bIsEdgeTypeArray = new Array();
     var siblingArray = new Array();
     var bIsSameSiblingArray = new Array();
+    var arcCircleAnalysisArray = new Array();    
     var bIsOnCircleArray = new Array();
     var bIsOnSplineArray = new Array();
 
     function getSelectionInfo()
     {
+
         // for each edge in the selection, get helpful information
         for (var j = 0; j < currentSelection.length; j++)
         {
             // if you're not in the Main History, need to calculate the depth to extract the correct history data
             var historyDepth = (currentSelection[j]["ids"].length) - 1;
 
-            // get objectID of the current selection
+            // get objectID of the current selection, then push the results into an array
             var nObjectID = currentSelection[j]["ids"][historyDepth]["Object"];
             //console.log("Selection ID: " + nObjectID);
+            nObjectIDArray.push(nObjectID);
+            //console.log("ObjectID array: " + nObjectIDArray);
 
             // get object type of the current selection, then push the results into an array
             var nType =  WSM.APIGetObjectTypeReadOnly(nHistoryID, nObjectID);
             //console.log("Object type: " + nType);
             typeArray.push(nType);
             //console.log("Object type array: " + typeArray);
+
+            // get vertexIDs of the current selection, then push the results into an array
+            var nVertexID = WSM.APIGetObjectsByTypeReadOnly(nHistoryID, nObjectID, WSM.nVertexType, false);
+            //console.log("nVertex ID: " + nVertexID);
+            nVertexIDArray.push(nVertexID);
+            //console.log("VertexID array: " + nVertexIDArray);
+
 
             function getArcCircleAnalysis() 
             {
@@ -51,7 +69,6 @@ deanstein.RebuildArcCircle = function(args)
                 var bIsOnCircle = arcCircleAnalysis["bHasCircleAttribute"];
                 //console.log("Is selection part of a circle? " + arcCircleAnalysis["bHasCircleAttribute"]);
                 bIsOnCircleArray.push(bIsOnCircle);
-                //console.log(JSON.stringify(arcCircleAnalysis));
                 arcCircleAnalysisArray.push(arcCircleAnalysis);
                 return arcCircleAnalysis;
             }
@@ -128,7 +145,7 @@ deanstein.RebuildArcCircle = function(args)
         // creates an array of boolean values depending on whether the selection contains edges
         function defineValidType()
         {
-            // the valid edge type is defined as the number 7
+            // the valid edge type is defined in WSM as the number 7
             var validType = 7;
             for (var m = 0; m < typeArray.length; m++)
             {
@@ -183,7 +200,6 @@ deanstein.RebuildArcCircle = function(args)
         console.log(preCheckPassed);
     }
 
-    // run the precheck to define the precheck variable
     var preCheckPassed = preCheck();
 
     // returns the type of operation to proceed with
@@ -225,21 +241,128 @@ deanstein.RebuildArcCircle = function(args)
         {
             console.log("\nBegin rebuild of arc or circle...\n");
 
+            // get the first index of the arc/circle analysis, which should be sufficient because we've already proven the arrays are identical by this point
             var arcCircleAnalysis = arcCircleAnalysisArray[0];
-            var center = arcCircleAnalysis["center"];
-            console.log("Center of circle: " + JSON.stringify(center));
+            //console.log("Arc/circle analysis to use as reference: " + JSON.stringify(arcCircleAnalysis));
 
-            var xAxis = arcCircleAnalysis["x-axis"];
-            console.log("X axis: " + JSON.stringify(xAxis));
+            var edgeCount = currentSelection.length;
+            console.log("Edges selected: " + edgeCount);
+
+            // get the ID of the first vertex of the first edge in the arraay
+            var arcStartPosID = nVertexIDArray[0][0];
+            console.log("Start point vertexID: " + arcStartPosID);
+
+            // get the point3D equivalent
+            var arcStartPos = WSM.APIGetVertexPoint3dReadOnly(nHistoryID, arcStartPosID);
+            console.log("Start point point3D: " + JSON.stringify(arcStartPos));
+
+            // get the ID of the last vertex of the last edge in the array
+            var arcEndPosID = nVertexIDArray[edgeCount - 1][1];
+            console.log("End point vertexID: " + arcEndPosID);
+
+            // get the point3D equivalent
+            var arcEndPos = WSM.APIGetVertexPoint3dReadOnly(nHistoryID, arcEndPosID);
+            console.log("End point point3D: " + JSON.stringify(arcEndPos));
+
+            function getMidPointAtFacetedCurve()
+            {
+                // check if the current edge count is odd or even, then get the midpoint
+                // if the edge count is even, simply take the point that naturally sits at the midpoint of the faceted arc
+                if ((edgeCount % 2) === 0)
+                {
+                    console.log("\nGetting midpoint based on even segment count...\n");
+                    var midPointEdgeIndex = edgeCount / 2 - 1;
+                    var midPointID = nVertexIDArray[midPointEdgeIndex][1];
+                    var midPointAtFacetedCurve = WSM.APIGetVertexPoint3dReadOnly(nHistoryID, midPointID);
+                    console.log("Midpoint based on even segment count: " + JSON.stringify(midPointAtFacetedCurve));
+                }
+                // or if the edge count is odd, calculate the midpoint of the middle segment
+                else
+                {
+                    console.log("\nGetting midpoint based on odd segment count...\n");
+                    var midPointEdgeIndex = Math.ceil(edgeCount / 2 - 1);
+                    console.log("midPointEdgeIndex: " + midPointEdgeIndex);
+
+                    // get the first point on the midpoint edge
+                    var midPointEdgePoint0ID = nVertexIDArray[midPointEdgeIndex][0];
+                    var midPointEdgePoint0 = WSM.APIGetVertexPoint3dReadOnly(nHistoryID, midPointEdgePoint0ID);
+                    var midPointEdgePoint0x = midPointEdgePoint0["x"];
+                    var midPointEdgePoint0y = midPointEdgePoint0["y"];
+                    var midPointEdgePoint0z = midPointEdgePoint0["z"];
+
+                    // get the last point on the midpoint edge
+                    var midPointEdgePoint1ID = nVertexIDArray[midPointEdgeIndex][1];
+                    var midPointEdgePoint1 = WSM.APIGetVertexPoint3dReadOnly(nHistoryID, midPointEdgePoint1ID);
+                    var midPointEdgePoint1x = midPointEdgePoint1["x"];
+                    var midPointEdgePoint1y = midPointEdgePoint1["y"];
+                    var midPointEdgePoint1z = midPointEdgePoint1["z"];
+                    //console.log("Got an x value?" + midPointEdgePoint1x);
+
+                    // generic function to get the midpoint between two points defined by an array [x,y,z]
+                    function getMidPointBetweenTwoPoints(x0,y0,z0, x1,y1,z1)
+                    {
+                        var x = (x0 + x1) / 2;
+                        var y = (y0 + y1) / 2;
+                        var z = (z0 + z1) / 2;
+
+                        var midPoint = new Array(x, y, z);
+                        // returns [x,y,z]
+                        return midPoint;
+                        console.log(midPoint);
+                    }
+
+                    var midPoint = getMidPointBetweenTwoPoints(midPointEdgePoint0x, midPointEdgePoint0y, midPointEdgePoint0z, midPointEdgePoint1x, midPointEdgePoint1y, midPointEdgePoint1z);
+                    var midPointAtFacetedCurve =  WSM.Geom.Point3d(midPoint[0], midPoint[1], midPoint[2]);
+                    
+                    // optionally draw a debug vertex that will appear when the midpoint is not coincident with the new curve
+                    var drawDebugPoint = false;
+                    if (drawDebugPoint === true)
+                    {
+                        WSM.APICreateVertex(nHistoryID, midPointAtFacetedCurve);
+
+                    }
+                    console.log("Midpoint based on odd segment count: " + JSON.stringify(midPointAtFacetedCurve));
+
+                }
+                return midPointAtFacetedCurve;
+                console.log("Midpoint at faceted curve: " + JSON.stringify(midPointAtFacetedCurve));
+            }
+
+            var midPointAtFacetedCurve = getMidPointAtFacetedCurve();
+
+            var accuracyORcount = args.facetCount;
+            var bReadOnly = false;
+            var trans;
+            var nMinimumNumberOfFacets = accuracyORcount;
+
+            // var radius = arcCircleAnalysis["radius"];
+            // console.log("Radius of circle: " + JSON.stringify(radius));
+            // var pi = 3.1415926535897932384626433832795;
+            // var circumference = radius * 2 * pi;
+            // console.log("Circumference of circle: " + JSON.stringify(circumference));
+
+            // if delete is checked, delete the original edges
+            var bDelete = true;
+            for (var n = 0; n < nObjectIDArray.length; n++)
+            {
+                if (bDelete === true) 
+                {
+                    WSM.APIDeleteObject(nHistoryID, nObjectIDArray[n]);
+                }
+            }
+
+            // execute the rebuild
+            WSM.APICreateCircleOrArcFromPoints(nHistoryID, arcStartPos, arcEndPos, midPointAtFacetedCurve, accuracyORcount, bReadOnly, trans, nMinimumNumberOfFacets);
+            console.log("\nCreated a new curve.");
 
         }
         else if (operationType === "spline")
         {
-            console.log("\n Rebuilding splines is not yet supported.");
+            console.log("\nRebuilding splines is not yet supported.");
         }
         else if (operationType === "line")
         {
-            console.log("\n Rebuilding lines is not yet supported");
+            console.log("\nRebuilding lines is not yet supported");
         }
     }
 
@@ -254,12 +377,56 @@ deanstein.RebuildArcCircle = function(args)
 
 
 
+deanstein.ExplodeCurve = function()
+{
+
+    FormIt.UndoManagement.BeginState();
+
+    console.clear();
+    
+    // get current history
+    var nHistoryID = FormIt.GroupEdit.GetEditingHistoryID();
+    console.log("Current history: " + JSON.stringify(nHistoryID));
+
+    // get current selection
+    var currentSelection = FormIt.Selection.GetSelections();
+    //console.log("Current selection: " + JSON.stringify(currentSelection));
+
+    // report how many items in the selection
+    var currentSelectionLength = currentSelection.length;
+    console.log("Number of objects in selection: " + currentSelectionLength);
+
+    if (currentSelection.length === 0)
+    {
+        console.log("Select a line, arc, circle, or spline to explode.");
+        return;
+    }
+
+    // for each edge in the selection, get the vertexIDs and mark them not smooth
+    for (var e = 0; e < currentSelection.length; e++)
+    {
+        // if you're not in the Main History, need to calculate the depth to extract the correct history data
+        var historyDepth = (currentSelection[e]["ids"].length) - 1;
+
+        var nObjectID = currentSelection[e]["ids"][historyDepth]["Object"];
+
+        // get vertexIDs of the current selection, then push the results into an array
+        var nVertexID = WSM.APIGetObjectsByTypeReadOnly(nHistoryID, nObjectID, WSM.nVertexType, false);
+        console.log("nVertex ID: " + nVertexID);
+
+        WSM.APISetEdgesOrVerticesMarkedSmooth(nHistoryID, nVertexID, false);
+    }
+    console.log("Exploded the curve.");
+
+    FormIt.UndoManagement.EndState("Explode Curve");
+}
+
+
+
 deanstein.CreateArcCircle = function(args)
 {
-    var radius = args.radius;
-    var posCenter = args.posCenter;
-    var arcLengthParameter = args.arcLengthParameter;
     var edgeLength = args.edgeLength;
+    var facetCount = args.facetCount;
     console.clear();
 
     // Get the current history
@@ -310,16 +477,29 @@ deanstein.Submit = function()
 {
     var args = 
     {
-    "radius": parseFloat(document.a.radius.value),
-    "posCenter": document.a.posCenter.value,
-    "arcLengthParameter": parseFloat(document.a.arcLengthParameter.value),
-    "edgeLength": parseFloat(document.a.edgeLength.value)
+    "edgeLength": parseFloat(document.a.edgeLength.value),
+    "facetCount": parseFloat(document.a.facetCount.value)
     }
 
-    console.log("deanstein.RebuildArcCircle");
+    console.log("deanstein.RebuildCurve");
     console.log("args");
     // NOTE: window.FormItInterface.CallMethod will call the function
     // defined above with the given args.  This is needed to communicate
     // between the web JS enging process and the FormIt process.
-    window.FormItInterface.CallMethod("deanstein.RebuildArcCircle", args);
+    window.FormItInterface.CallMethod("deanstein.RebuildCurve", args);
+}
+
+deanstein.SubmitExplode = function(argsExplode)
+{
+
+    var argsExplode =
+    {
+
+    }
+    console.log("deanstein.RebuildCurve");
+    console.log("args");
+    // NOTE: window.FormItInterface.CallMethod will call the function
+    // defined above with the given args.  This is needed to communicate
+    // between the web JS enging process and the FormIt process.
+    window.FormItInterface.CallMethod("deanstein.ExplodeCurve", argsExplode);
 }

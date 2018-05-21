dd528d68b1887de0afd4cf9c3a8d9c8c4eaf5493
Josh Goldstein <josh.goldstein@autodesk.com>
2018-05-21 15:02:49 -0600

    Improved UX and reliability of Extract Material Textures
    - FIxed issue where images would be continuously added to DOM
    - Fixes for scenarios where no texture, material, or selection is present
    - Broke JS files up into "Client" and "Web" versions for clarity

--- ExtractMaterialTextures/ExtractMaterialTextures_Client.js
+++ ExtractMaterialTextures/ExtractMaterialTextures_Client.js
@@ -15,6 +15,12 @@ deanstein.ExtractMaterialTextures = function(args)
     var currentSelection = FormIt.Selection.GetSelections();
     console.log("Current selection: " + JSON.stringify(currentSelection));
 
+    if (currentSelection.length < 1)
+    {
+        console.log("Nothing was selected.");
+        return undefined;
+    }
+
     // if you're not in the Main History, calculate the depth to extract the correct history data
     var historyDepth = (currentSelection[0]["ids"].length) -1;
     console.log("Current history depth: " + historyDepth);
@@ -26,46 +32,33 @@ deanstein.ExtractMaterialTextures = function(args)
     // get the material ID for the selection
     var selectionMaterialID = WSM.APIGetObjectMaterialReadOnly(nHistoryID, nObjectID);
     console.log("Current selection material ID: " + selectionMaterialID);
+    // if the WSM Invalid ID is detected, throw an error and return undefined
+    if (selectionMaterialID === 4294967295)
+    {
+        console.log("This geometry has no Material applied to it.");
+        return undefined;
+    }
 
+    // get the material data for the selection
     var selectionMaterialData = WSM.APIGetMaterialDataReadOnly(nHistoryID, selectionMaterialID);
     console.log("Current selection material data: " + JSON.stringify(selectionMaterialData));
 
+    // get the texture ID for the material
+    var selectionTextureID = selectionMaterialData.nTextureID;
+    console.log("Selection textureID: " + selectionTextureID);
+    // if the WSM Invalid ID is detected, throw an error and return undefined
+    if (selectionTextureID === 4294967295)
+    {
+        console.log("This Material has no texture associated with it.");
+        return undefined;
+    }
+
     // get the texture data
-    var textureData = WSM.APIGetTextureDataReadOnly(nHistoryID, selectionMaterialData.nTextureID);
+    var textureData = WSM.APIGetTextureDataReadOnly(nHistoryID, selectionTextureID);
     //console.log("Current selection texture data: " + JSON.stringify(textureData));
 
     var bitmapData = textureData.bitmap;
-    console.log("bitmap data: " + JSON.stringify(bitmapData));
+    //console.log("bitmap data: " + JSON.stringify(bitmapData));
 
     return bitmapData;
-}
-
-// Submit runs from the HTML page.  This script gets loaded up in both FormIt's
-// JS engine and also in the embedded web JS engine inside the panel.
-deanstein.Submit = function()
-{
-    var args = {
-    //"radius": parseFloat(document.a.radius.value),
-    //"cleanup": document.a.cleanup.checked
-    }
-
-    console.log("deanstein.ExtractMaterialTextures");
-    console.log("args");
-    // NOTE: window.FormItInterface.CallMethod will call the function
-    // defined above with the given args.  This is needed to communicate
-    // between the web JS enging process and the FormIt process.
-    window.FormItInterface.CallMethod("deanstein.ExtractMaterialTextures", args);
-    FormItInterface.CallMethod("deanstein.ExtractMaterialTextures", args,
-        function(bitmapData)
-        {
-            //debugger;
-            var evalBitmapData = eval(bitmapData);
-            var data = new Uint8Array(evalBitmapData);
-            var blob = new Blob([data], {type: 'image/bmp'});
-            var imgURL = URL.createObjectURL(blob);
-
-            var img = document.createElement("img");
-            img.src = imgURL;
-            window.document.body.appendChild(img);
-        });
 }
